b8900c0a437169b76b85e6f49e3439a9b9cea333
coryo12345 <corydio12@gmail.com>
2020-04-01 21:43:07 -0400

    can upload a file! given gcp json exists

--- .gcp/storage.sh
+++ .gcp/storage.sh
@@ -2,4 +2,6 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+echo "$DIR/storage.json"
+
 export GOOGLE_APPLICATION_CREDENTIALS="$DIR/storage.json"
--- webservice/controllers/fileController.js
+++ webservice/controllers/fileController.js
@@ -2,6 +2,13 @@
 *   auth Controller
 */
 
+var tmp = require('tmp');
+const fs = require('fs');
+const fspath = require('path');
+const uuidv1 = require('uuid/v1');
+const { Storage } = require('@google-cloud/storage');
+const storage = new Storage();
+
 const { orgDao } = require('../models/orgDao');
 
 /** 
@@ -17,9 +24,28 @@ exports.upload = function (req, res) {
     }
     let entity = req.session.entity;
     console.log("file: ", req.file);
-    // get file from req
-    // get entity , page , name
-    // construct file path
-    // send request w/ file to storage_controller
-    res.json({});
+
+    tmp.dir(function _tempDirCreated(err, path, cleanupCallback) {
+        if (err) throw err;
+        filepath = fspath.resolve(path, req.file.originalname);
+        fs.writeFileSync(filepath, req.file.buffer);
+
+        // get entity , page , file_id
+        let remotepath = entity + '/' + 'page1' + '/' + req.file.originalname;
+        // construct bucket file path
+
+
+        storage.bucket('orgresource-0').upload(filepath, {
+            destination: remotepath,
+            gzip: true,
+            metadata: {
+                //cacheControl: 'public, max-age=31536000',
+                cacheControl: 'no-cache'
+            }
+        }).catch((err) => { console.error(err) });
+
+        // Manual cleanup
+        cleanupCallback();
+        res.json({});
+    });
 }
